eca3039547759e11e8ac25e1ac271866b598161c
Avi074 <gautamavinash.ag@gmail.com>
2025-02-27 19:10:15 +0530

    user image optimised

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,15 +2,16 @@
 
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
+import Image from "next/image";
 
 export default function Profile() {
   const user = useSelector((state: RootState) => state.user);
 
   return (
     <div>
-      <h1 className="text-3xl font-bold">👤 {user.name}'s Profile</h1>
+      <h1 className="text-3xl font-bold">👤 {user.name}&apos;s Profile</h1>
       <p>{user.bio}</p>
-      <img src={user.profilePicture} className="w-24 h-24 rounded-full mt-4" />
+      <Image src={user.profilePicture} alt={user.name} className="w-24 h-24 rounded-full mt-4" />
     </div>
   );
 }
